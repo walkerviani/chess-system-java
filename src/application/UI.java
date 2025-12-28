9779e4a679c718ff3bdbd94e3f5d56732d5b8170
Walker Viani <walkeryslan@gmail.com>
2025-12-28 08:20:43 -0300

    "Switching player each turn"

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -44,6 +45,13 @@ public class UI {
         } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading chess position. Valid values are from a1 to h8.");
         }
+    }
+
+    public static void printMatch(ChessMatch ChessMatch) {
+        printBoard(ChessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " +  ChessMatch.getTurn());
+        System.out.println("Waiting player: " +  ChessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
