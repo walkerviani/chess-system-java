91bab9226c4c6eec4ac679c7749e565fb8f521d8
Walker Viani <walkeryslan@gmail.com>
2025-12-28 21:00:20 -0300

    "Checkmate logic"

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -41,6 +41,7 @@ public class Program {
                 sc.nextLine();
             }
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
